3ed5772181e4bb8abd3c7acc68d84a351b6cc5bd
hirey-m <hirey.m@northeastern.edu>
2022-09-28 23:01:43 -0400

    Initial Commit

---
+++ HumanResourceOfficer/src/employeeModel/Employee.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employeeModel;
+
+/**
+ *
+ * @author manavhirey
+ */
+public class Employee {
+    
+    private String name;
+    private int employeeId;
+    private int age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String position;
+    private int cellNo;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getCellNo() {
+        return cellNo;
+    }
+
+    public void setCellNo(int cellNo) {
+        this.cellNo = cellNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
---
+++ HumanResourceOfficer/src/employeeModel/EmployeeDirectory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employeeModel;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author manavhirey
+ */
+public class EmployeeDirectory {
+    
+    private ArrayList<Employee> directory;
+    
+    public EmployeeDirectory(){
+        this.directory = new ArrayList<Employee>();    
+    }
+
+    public ArrayList<Employee> getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(ArrayList<Employee> directory) {
+        this.directory = directory;
+    }
+    
+    
+}
